6435fe1d841e0c0c87e72e4823d43d6c651723af
hibohiboo <hibohiboo66+github@gmail.com>
2026-05-10 13:48:05 +0900

    fix css error AB#527

--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -22,5 +22,10 @@ export default defineConfig({
     exclude: [...configDefaults.exclude, 'e2e/**'],
     root: fileURLToPath(new URL('./', import.meta.url)),
     setupFiles: ['./vitest.setup.ts'],
+    server: {
+      deps: {
+        inline: ['vuetify'],
+      },
+    },
   },
 })
